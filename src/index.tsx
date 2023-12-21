bcec86150ed23445b52e3523fd70184a32a4e6b1
Subhan Ali <subhan25902@gmail.com>
2023-12-21 17:16:39 +0500

    add rollup

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,2 @@
-// export * from "./Card/Card";
-// export * from './filter/Filter';
-import Filter from "./filter/Filter";
-export default Filter;
+export * from "./Card/Card";
+export * from './filter/Filter';
