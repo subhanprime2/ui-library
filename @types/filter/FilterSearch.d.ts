22adbdcf7a8d0c22b157e4ca7a4d9753ad60ada7
Subhan Ali <subhan25902@gmail.com>
2023-12-21 12:04:10 +0500

    add dependency

--- @types/filter/FilterSearch.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-interface SearchProps {
-    getLength: () => string;
-    searchValue: string;
-    onChange: (e: any) => void;
-}
-declare function FilterSearch({ getLength, searchValue, onChange }: SearchProps): React.JSX.Element;
-export default FilterSearch;
