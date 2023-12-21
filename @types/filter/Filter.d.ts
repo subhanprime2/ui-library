4ed7cfb5e8e22ca59c9b6c5bfc51fafff93a4e48
Subhan Ali <subhan25902@gmail.com>
2023-12-21 12:09:17 +0500

    create build of v..5

---
+++ @types/filter/Filter.d.ts
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../index.css';
+interface Data {
+    tasks: TaskMap | null;
+    projects: ProjectsAllMap | null;
+    people: PeopleMap | null;
+    peopleTasks: PeopleTasks[] | null;
+    projectTasks: ProjectTask[] | null;
+    roles: Role[] | null;
+    department: Department[] | null;
+    tags: Tag[] | null;
+    projectOwner: People[] | null;
+    managers: People[] | null;
+}
+interface FilterProps {
+    sheduler: Data;
+    filters: FilterType[];
+    selectedFilter: SelectedFilterType[];
+    resetFilters: () => void;
+    clearSelectedFilters: () => void;
+    addSelectedFilter: (data: SelectedFilterType) => void;
+    updateSelectedFilter: (index: number, data: SelectedFilterType) => void;
+    deleteSelectedFilter: (index: number) => void;
+}
+declare function Filter({ sheduler, filters, selectedFilter, resetFilters, clearSelectedFilters, addSelectedFilter, updateSelectedFilter, deleteSelectedFilter, }: FilterProps): React.JSX.Element;
+export default Filter;
